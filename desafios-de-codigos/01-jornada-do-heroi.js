ea0edc913203413ce0fd072faa31c8e787b699c7
André Storti Andrade <a.storti@hotmail.com>
2025-09-02 17:28:55 -0300

    desafios de códigos

---
+++ desafios-de-codigos/01-jornada-do-heroi.js
@@ -0,0 +1,8 @@
+const posicaoInicial = 15;
+const totalPassos = 7;
+
+//TODO: Calcule a posição final do herói:
+const posicaoFinal = posicaoInicial + totalPassos;
+
+// Imprime a posição final
+console.log("Posicao final do heroi:", posicaoFinal);
---
+++ desafios-de-codigos/02-pontos-de-experiencia.js
@@ -0,0 +1,8 @@
+let num1 = 15;
+let num2 = 20;
+
+//TODO: Implemente a lógica para exibir o xpGanho:
+const xpGanho = num1 * num2 * 100;
+
+// Imprime a quantidade de XP ganho
+console.log("Voce ganhou " + xpGanho + " XP!");
